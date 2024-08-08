6227b12ed7d6205aec131a9785aa2fa0d44d57d0
Eric Zhou <ericzhou1212@gmail.com>
2024-08-07 19:17:27 -0700

    completed

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from './features/header/Header'
+import TodoList from './features/todos/TodoList'
+import Footer from './features/footer/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+          <section className="medium-container">
+          <h2>Todos</h2>
+          <div className="todoapp">
+            <Header />
+            <TodoList />
+            <Footer />
+          </div>
+        </section>
+      </main>
+      <section>
+        <h2>Welcome to the Redux Fundamentals example app!</h2>
+      </section>
+    </div>
+  )
+}
+
+export default App
